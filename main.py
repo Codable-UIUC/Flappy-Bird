96676a44b69a085670e93f5661d63d42a27613c0
mbhang2 <mbhang2@illinois.edu>
2022-12-06 18:23:31 -0600

    Added most of options and bgm/sfx

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import sys 
 import pygame
 from pygame.locals import *
+from pygame import mixer
 
 width = 289
 height = 511
 framepersecond = 32
+
+# Screen and buttons
 screen = pygame.display.set_mode((width, height))
 background = pygame.transform.scale(pygame.image.load("background.jpeg"), (width, height))
 play_button = pygame.transform.scale(pygame.image.load("buttons/play.png"), (150, 70))
-option_button = pygame.transform.scale(pygame.image.load("buttons/options.png"), (100, 60))
-quit_button = pygame.transform.scale(pygame.image.load("buttons/icon_x.png"), (57, 57))
+option_button = pygame.transform.scale(pygame.image.load("buttons/options.png"), (120, 60))
+quit_button = pygame.transform.scale(pygame.image.load("buttons/exit.png"), (120, 60))
+home_button = pygame.transform.scale(pygame.image.load("buttons/icon_home.png"), (45, 45))
+
+# Options
+play_bgm = True
+play_sfx = True
+
 player = pygame.transform.scale(pygame.image.load("player.png"), (32, 32)) # <-- placeholder
+
+
 
 groundY = height * 0.9
 
@@ -26,10 +37,10 @@ def homescreen():
         screen.blit(play_button, (70, 300))
 
         # option button
-        screen.blit(option_button, (40, 400))
+        screen.blit(option_button, (15, 400))
 
         # quit button
-        screen.blit(quit_button, (190, 403))
+        screen.blit(quit_button, (155, 400))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -39,10 +50,10 @@ def homescreen():
                 if mouse_pos[0] in range(70, 220) and mouse_pos[1] in range(300, 370):
                     # game start
                     return
-                if mouse_pos[0] in range(40, 110) and mouse_pos[1] in range(400, 470):
+                if mouse_pos[0] in range(15, 135) and mouse_pos[1] in range(400, 460):
                     # options
-                    pass
-                if mouse_pos[0] in range(160, 260) and mouse_pos[1] in range(403, 463):
+                    optionscreen()
+                if mouse_pos[0] in range(155, 275) and mouse_pos[1] in range(400, 460):
                     # exit
                     pygame.quit()
                     sys.exit()
@@ -51,7 +62,68 @@ def homescreen():
 
 
 def optionscreen():
-    pass
+    while True:
+        global play_bgm
+        global play_sfx
+        screen.blit(background, (0, 0))
+        mouse_pos = pygame.mouse.get_pos()
+
+        # Title
+        title = pygame.font.Font('freesansbold.ttf', 45).render('Options', True, (255, 255, 255))
+        screen.blit(title, (55, 75))
+
+        # BGM
+        bgm_text = pygame.font.Font('freesansbold.ttf', 30).render('BGM', True, (255, 255, 255))
+        screen.blit(bgm_text, (30, 210))
+
+        # SFX
+        sfx_text = pygame.font.Font('freesansbold.ttf', 30).render('SFX', True, (255, 255, 255))
+        screen.blit(sfx_text, (30, 270))
+
+        on_text_on = pygame.font.Font('freesansbold.ttf', 30).render('ON', True, (0, 255, 0))
+        off_text_on = pygame.font.Font('freesansbold.ttf', 30).render('OFF', True, (255, 0, 0))
+        on_text_off = pygame.font.Font('freesansbold.ttf', 30).render('ON', True, (0, 75, 0))
+        off_text_off = pygame.font.Font('freesansbold.ttf', 30).render('OFF', True, (75, 0, 0))
+
+        if play_bgm:
+            screen.blit(on_text_on, (130, 210))
+            screen.blit(off_text_off, (200, 210))
+
+        elif not play_bgm:
+            screen.blit(on_text_off, (130, 210))
+            screen.blit(off_text_on, (200, 210))
+
+        if play_sfx:
+            screen.blit(on_text_on, (130, 270))
+            screen.blit(off_text_off, (200, 270))
+
+        elif not play_sfx:
+            screen.blit(on_text_off, (130, 270))
+            screen.blit(off_text_on, (200, 270))
+
+
+        # Back Button
+        screen.blit(home_button, (5, 5))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if mouse_pos[0] in range(5, 50) and mouse_pos[1] in range(5, 50):
+                    return
+                if mouse_pos[0] in range(130, 180) and mouse_pos[1] in range(210, 240):
+                    play_bgm = True
+                if mouse_pos[0] in range(200, 260) and mouse_pos[1] in range(210, 240):
+                    play_bgm = False
+                if mouse_pos[0] in range(130, 180) and mouse_pos[1] in range(270, 300):
+                    play_sfx = True
+                if mouse_pos[0] in range(200, 260) and mouse_pos[1] in range(270, 300):
+                    play_sfx = False
+
+
+        pygame.display.update()
+
 
 
 def gamescreen():
